0834c5a3bfef7dde7da411ada15163bfb2412caa
prakash <prakashvadakadu8@gmail.com>
2025-07-20 09:36:48 +0530

    chore: stop tracking .env file

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const port = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://twitter-client-three.vercel.app",
+  "https://twitter-client-git-main-prakashs-projects-296c4bec.vercel.app/",
 ];
 
 app.use(
