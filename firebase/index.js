77f313149ec97cd57054d75ce8823fe893c4ffd5
prakash <prakashvadakadu8@gmail.com>
2025-07-24 20:49:31 +0530

    firebase config setup cmt

--- firebase/index.js
+++ firebase/index.js
@@ -1,5 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-config.json" assert { type: "json" };
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const serviceAccount = JSON.parse(
+  readFileSync(join(__dirname, "./firebase-config.json"), "utf-8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
