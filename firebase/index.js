4014bd4285a2462b10ed65b70d987af4869d7edd
prakash <prakashvadakadu8@gmail.com>
2025-07-24 23:44:32 +0530

    config setup set

--- firebase/index.js
+++ firebase/index.js
@@ -1,5 +1,6 @@
 import admin from "firebase-admin";
-
+import dotenv from "dotenv";
+dotenv.config();
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
